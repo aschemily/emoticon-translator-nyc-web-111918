a8d2b096837c4d2b9291f9c7cd571d2571eab7e7
Emily Asch <e.asch@me.com>
2018-11-06 00:46:01 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,23 +12,23 @@ def load_library(file_path)
   result
 end
 
-def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
-  # code goes here
-  library = load_library(file_path)
-  if library["get_emoticon"].include?(english_emoticon)
-    library["get_emoticon"][english_emoticon]
-  else
-    "Sorry, that emoticon was not found"
-  end
+# def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
+#   # code goes here
+#   library = load_library(file_path)
+#   if library["get_emoticon"].include?(english_emoticon)
+#     library["get_emoticon"][english_emoticon]
+#   else
+#     "Sorry, that emoticon was not found"
+#   end
     
-end
+# end
 
-def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
-  # code goes here
-  library = load_library(file_path)
-  if library["get_meaning"].include?(japanese_emoticon)
-    library["get_meaning"][japanese_emoticon]
-  else
-    "Sorry, that emoticon was not found"
-  end
-end
+# def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
+#   # code goes here
+#   library = load_library(file_path)
+#   if library["get_meaning"].include?(japanese_emoticon)
+#     library["get_meaning"][japanese_emoticon]
+#   else
+#     "Sorry, that emoticon was not found"
+#   end
+# end
